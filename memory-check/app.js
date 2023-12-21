a814882f7358365f814a2f3ac6b911138438bd9d
1 <menathlakvindub@gmail.com>
2023-12-21 21:10:02 +0530

    creted grid \ memory check

--- memory-check/app.js
+++ memory-check/app.js
@@ -1,1 +1,70 @@
+const scoreDisplay = document.getElementById("score");
+const gridDisplay = document.querySelector("#grid");
+
+const cardArray = [
+    {
+        name: "fries",
+        img: "images/fries.png"
+    },
+    {
+        name: "cheeseburger",
+        img: "images/cheeseburger.png"
+    },
+    {
+        name: "hotdog",
+        img: "images/hotdog.png"
+    },
+    {
+        name: "ice-cream",
+        img: "images/ice-cream.png"
+    },
+    {
+        name: "milkshake",
+        img: "images/milkshake.png"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.png"
+    },
+    {
+        name: "fries",
+        img: "images/fries.png"
+    },
+    {
+        name: "cheeseburger",
+        img: "images/cheeseburger.png"
+    },
+    {
+        name: "hotdog",
+        img: "images/hotdog.png"
+    },
+    {
+        name: "ice-cream",
+        img: "images/ice-cream.png"
+    },
+    {
+        name: "milkshake",
+        img: "images/milkshake.png"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.png"
+    }
+];
+
+cardArray.sort(() => 0.5 - Math.random())
+
+function createBoard() {
+    for (let i=0; i < cardArray.length; i++) {
+        const card = document.createElement("img");
+        card.setAttribute("src","images/blank.png");
+        card.setAttribute("dataId",i);
+        gridDisplay.appendChild(card)
+
+    }
+    
+}
+
+createBoard();
+
 
